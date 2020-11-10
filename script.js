aea41d7cccfe2f021b7e6a7b76ad04a19a28ac0d
Epi Martinez <epimartinez25@gmail.com>
2020-11-09 19:42:22 -0500

    Vali is awesome

---
+++ script.js
@@ -0,0 +1,28 @@
+function idCard() {
+var firstName = document.getElementById("firstName").value;
+var lastName = document.getElementById("lastName").value;
+var address = document.getElementById("address").value;
+var age = parseInt(document.getElementById("age").value);
+var phoneNumber = parseInt(document.getElementById("phoneNumber").value);
+
+var numberArray = [];
+
+numberArray.push(age);
+numberArray.push(phoneNumber);
+
+for (var i = 0; i <= numberArray.length; i++) {
+    if (numberArray[i] <= 100){
+        document.getElementById("postAge").innerHTML = "Age: " + age;
+        
+    }
+
+    else if (numberArray[i] > 100){
+        document.getElementById("postPhoneNumber").innerHTML = "PhoneNumber: " + phoneNumber;
+        
+    }
+
+document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
+document.getElementById("postAddress").innerHTML = address;
+
+}
+}
